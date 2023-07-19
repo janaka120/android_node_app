c1450a894280d1f6f8af893501fb8b79f8623a99
janaka120 <janakastill@gmail.com>
2023-07-19 22:59:42 +0800

    Update note doa interface

--- app/src/main/java/com/example/noteapplication/Room/NoteDOA.kt
+++ app/src/main/java/com/example/noteapplication/Room/NoteDOA.kt
@@ -1,7 +1,25 @@
 package com.example.noteapplication.Room
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.noteapplication.Model.Note
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDOA {
+    @Insert
+    suspend fun insert(note: Note)
+    @Update
+    suspend fun update(note: Note)
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("DELETE FROM note_table")
+    suspend fun deleteAllNotes()
+
+    @Query("SELECT * FROM note_table ORDER BY id ASC")
+    fun getAllNotes(): Flow<List<Note>>
 }
